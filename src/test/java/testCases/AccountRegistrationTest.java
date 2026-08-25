2ae77187007606e40c4bcf2bc0a7764e5f2f3ced
Ankit Tripathi <tripathi.ankit83@gmail.com>
2026-08-25 11:37:37 +0530

    Refactor account registration test with step reporting
    Enhanced the account registration test with detailed steps and reporting.

--- src/test/java/testCases/AccountRegistrationTest.java
+++ src/test/java/testCases/AccountRegistrationTest.java
@@ -1,27 +1,41 @@
 package testCases;
 
-import testBase.BaseClass;
-
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import org.testng.Assert;
 
 import pageObjects.HomePage;
+import testBase.BaseClass;
 import utilities.ExtentReportListener;
-
 
 @Listeners(ExtentReportListener.class)
 public class AccountRegistrationTest extends BaseClass {
-	
-	@Test
-	public void verify_account_registration() {
-		HomePage hp = new HomePage(driver);
-		hp.clickOnMyAccountDrp();
-		hp.selectRegisterOptn();
-		
-		String getTtile = driver.getTitle();
-		
-		Assert.assertEquals(getTtile, "Register Account");
-	}
 
+    @Test
+    public void verify_account_registration() {
+
+        HomePage hp = new HomePage(driver);
+
+        // Step 1
+        hp.clickOnMyAccountDrp();
+
+        ExtentReportListener.getTest()
+                .pass("Click My Account");
+
+        // Step 2
+        hp.selectRegisterOptn();
+
+        ExtentReportListener.getTest()
+                .pass("Select Register option");
+
+        // Step 3
+        String getTitle = driver.getTitle();
+
+        Assert.assertEquals(
+                getTitle,
+                "Register Account");
+
+        ExtentReportListener.getTest()
+                .pass("Verify Register Account title");
+    }
 }
